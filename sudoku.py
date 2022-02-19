4891242f7601432ec7d2f77e387ce7dcdece25bb
jvandalsem <jjvandal@umich.edu>
2022-02-19 14:23:45 -0800

    Reformat board, added Squares class

--- sudoku.py
+++ sudoku.py
@@ -1,18 +1,31 @@
 #Soduko Solver
 import random
-board = [[[' ',' ',' '],[7,9,' '],[' ',5,' ']],
-         [[3,5,2],[' ',' ',8],[' ',4,' ']],
-         [[' ',' ',' '],[' ',' ',' '],[' ',8,' ']],
-         [[' ',1,' '],[' ',7,' '],[' ',' ',4]],
-         [[6,' ',' '],[3,' ',1],[' ',' ',8]],
-         [[9,' ',' '],[' ',8,' '],[' ',1,' ']],
-         [[' ',2,' '],[' ',' ',' '],[' ',' ',' ']],
-         [[' ',4,' '],[5,' ',' '],[8,9,1]],
-         [[' ',8,' '],[' ',3,7],[' ',' ',' ']]]
+board = [[' ',' ',' ',7,9,' ',' ',5,' '],
+         [3,5,2,' ',' ',8,' ',4,' '],
+         [' ',' ',' ',' ',' ',' ',' ',8,' '],
+         [' ',1,' ',' ',7,' ',' ',' ',4],
+         [6,' ',' ',3,' ',1,' ',' ',8],
+         [9,' ',' ',' ',8,' ',' ',1,' '],
+         [' ',2,' ',' ',' ',' ',' ',' ',' '],
+         [' ',4,' ',5,' ',' ',8,9,1],
+         [' ',8,' ',' ',3,7,' ',' ',' ']]
 
 def printBoard(board):
     for a in board:
-        print('[{}|{}|{}][{}|{}|{}][{}|{}|{}|]'.format(a[0][0],a[0][1],a[0][2],
-                                                       a[1][0],a[1][1],a[1][2],
-                                                       a[2][0],a[2][1],a[2][2]))
+        print('[{}|{}|{}][{}|{}|{}][{}|{}|{}|]'.format(a[0],a[1],a[2],
+                                                       a[3],a[4],a[5],
+                                                       a[6],a[7],a[8]))
 printBoard(board)
+class Squares:
+    def __init__(self,square,x,y):
+        self.value = square
+        self.position = (x,y)
+
+squareObjects = []
+for row in board:
+    for square in row:
+        squareObjects.append(Squares(square,board.index(row),row.index(square)))
+        # for square in tri:
+        #     squareObjects.append(Squares(row,square))
+for squares in squareObjects:
+    print(squares.value,squares.position)
